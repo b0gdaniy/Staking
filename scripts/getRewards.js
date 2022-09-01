8961971d98f07b38a59134161a7c3be0751c9679
r0uge-exe <pukhnobg@gmail.com>
2022-09-01 22:46:32 +0300

    12 commit

--- scripts/getRewards.js
+++ scripts/getRewards.js
@@ -41,40 +41,52 @@ async function main() {
 		owner
 	)
 
+	const stakingContractSFTBalanceBefore = await selfFarmContract.balanceOf(sRContract.address)
+	console.log(sRContract.address, " Address SFT token balance before: ", stakingContractSFTBalanceBefore, " tokens")
+
+	console.log("-----------------------------------------------------------------------------------")
+
 	// rewards for 3 stakers
 	const staker1getRewards = await sRContract.connect(staker1).getRewards({ gasLimit: 3e7 })
 	const staker1receipt = await staker1getRewards.wait()
-	// const staker1event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker1receipt.blockHash)
-	// console.log("Staker1 Stake Event: ", staker1event)
-	checkBalanceFor(staker1.address)
+	// // const staker1event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker1receipt.blockHash)
+	// // console.log("Staker1 Stake Event: ", staker1event)
+
+	// check balance for {staker1}
+	const staker1SFTBalance = await selfFarmContract.balanceOf(staker1.address)
+	console.log(staker1.address, " Address SFT token balance: ", staker1SFTBalance, " tokens")
+
+	console.log("-----------------------------------------------------------------------------------")
 
 	const staker2getRewards = await sRContract.connect(staker2).getRewards({ gasLimit: 3e7 })
 	const staker2receipt = await staker2getRewards.wait()
-	// const staker2event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker2receipt.blockHash)
-	// console.log("Staker2 Stake Event: ", staker2event)
-	checkBalanceFor(staker2.address)
+	// // const staker2event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker2receipt.blockHash)
+	// // console.log("Staker2 Stake Event: ", staker2event)
+	// check balance for {staker2}
+	const staker2SFTBalance = await selfFarmContract.balanceOf(staker2.address)
+	console.log(staker2.address, " Address SFT token balance: ", staker2SFTBalance, " tokens")
+
+	console.log("-----------------------------------------------------------------------------------")
 
 	const staker3getRewards = await sRContract.connect(staker3).getRewards({ gasLimit: 3e7 })
 	const staker3receipt = await staker3getRewards.wait()
-	// const staker3event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker3receipt.blockHash)
-	// console.log("Staker3 Stake Event: ", staker3event)
-	checkBalanceFor(staker3.address)
+	// // const staker3event = await sRContract.queryFilter(sRContract.filters.Deposit(), staker3receipt.blockHash)
+	// // console.log("Staker3 Stake Event: ", staker3event)
+	// check balance for {staker3}
+	const staker3SFTBalance = await selfFarmContract.balanceOf(staker3.address)
+	console.log(staker3.address, " Address SFT token balance: ", staker3SFTBalance, " tokens")
 
-	checkBalanceFor(sRContract.address)
+	console.log("-----------------------------------------------------------------------------------")
+
+	const stakingContractSFTBalanceAfter = await selfFarmContract.balanceOf(sRContract.address)
+	console.log(sRContract.address, " Address SFT token balance after: ", stakingContractSFTBalanceAfter, " tokens")
+
+	// // const addressONEBalanceS = await tokenOneContract.balanceOf(sRContract.address)
+	// // console.log(sRContract.address, " Address ONE token balance: ", addressONEBalanceS)
+
+	// // const addressTWOBalanceS = await tokenTwoContract.balanceOf(sRContract.address)
+	// // console.log(sRContract.address, " Address TWO token balance: ", addressTWOBalanceS)
 }
-
-// check balance for {address}
-async function checkBalanceFor(_address) {
-	const addressSFTBalance = await selfFarmContract.balanceOf(_address.address)
-	console.log(_address, " Address SFT token balance: ", addressSFTBalance)
-
-	const addressONEBalance = await tokenOneContract.balanceOf(_address.address)
-	console.log(_address, " Address ONE token balance: ", addressONEBalance)
-
-	const addressTWOBalance = await tokenTwoContract.balanceOf(_address.address)
-	console.log(_address, " Address TWO token balance: ", addressTWOBalance)
-}
-
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
