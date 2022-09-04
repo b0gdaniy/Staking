93efbf627c380e83a496c37c1c773b1d6005eabf
r0uge-exe <pukhnobg@gmail.com>
2022-09-05 00:09:44 +0300

    test code refactoring

--- test/staking.test.js
+++ test/staking.test.js
@@ -17,13 +17,10 @@ describe("StakingRewards", () => {
 
 		// deploy {SFT}
 		erc20Sft = await deployErc20(owner, "SelfFarmToken", "SFT")
-
 		// deploy {ONE}
 		erc20One = await deployErc20(owner, "TokenOne", "ONE")
-
 		// deploy {TWO}
 		erc20Two = await deployErc20(owner, "TokenTwo", "TWO")
-
 		// deploy {StakingRewards}
 		const Staking = await ethers.getContractFactory("StakingRewards", owner)
 		staking = await Staking.deploy(erc20Sft.address, erc20One.address, erc20Two.address)
@@ -36,9 +33,9 @@ describe("StakingRewards", () => {
 		expect(erc20Two.address).to.be.properAddress
 	})
 	it("have 0 any tokens by default", async () => {
-		const staker1balance = await tokensBalance(staker1.address, 1, erc20Sft, erc20One, erc20Two)
-		const staker2balance = await tokensBalance(staker2.address, 2, erc20Sft, erc20One, erc20Two)
-		const staker3balance = await tokensBalance(staker3.address, 3, erc20Sft, erc20One, erc20Two)
+		const staker1balance = await tokensBalance(staker1.address)
+		const staker2balance = await tokensBalance(staker2.address)
+		const staker3balance = await tokensBalance(staker3.address)
 
 		for (let i = 0; i < 3; ++i) {
 			expect(staker1balance[i]).to.eq(0)
@@ -47,83 +44,51 @@ describe("StakingRewards", () => {
 		}
 	})
 	it("possible to mint any tokens", async () => {
-		const staker1BalanceAfterMint = await mintTokens(staker1.address, 1, erc20Sft, erc20One, erc20Two, tokensAmount)
-		const staker2BalanceAfterMint = await mintTokens(staker2.address, 2, erc20Sft, erc20One, erc20Two, tokensAmount)
-		const staker3BalanceAfterMint = await mintTokens(staker3.address, 3, erc20Sft, erc20One, erc20Two, tokensAmount)
+		const staker1BalanceAfterMint = await mintTokens(staker1.address, tokensAmount)
 
 		for (let i = 0; i < 3; ++i) {
 			expect(staker1BalanceAfterMint[i]).to.eq(tokensAmount)
-			expect(staker2BalanceAfterMint[i]).to.eq(tokensAmount)
-			expect(staker3BalanceAfterMint[i]).to.eq(tokensAmount)
 		}
 	})
 	it("possible to enter staking with any tokens", async () => {
 		// mint {tokensAmount} SFT, ONE, TWO tokens to stakers
-		const staker1Sft = await erc20Sft.mint(staker1.address, tokensAmount)
-		await staker1Sft.wait()
-		const staker2One = await erc20One.mint(staker2.address, tokensAmount)
-		await staker2One.wait()
-		const staker3Two = await erc20Two.mint(staker3.address, tokensAmount)
-		await staker3Two.wait()
+		await mintTokens(staker1.address, tokensAmount)
 
-		const sftAllowance = await erc20Sft.connect(staker1).increaseAllowance(staking.address, tokensAmount)
-		await sftAllowance.wait()
-		const oneAllowance = await erc20One.connect(staker2).increaseAllowance(staking.address, tokensAmount)
-		await oneAllowance.wait()
-		const twoAllowance = await erc20Two.connect(staker3).increaseAllowance(staking.address, tokensAmount)
-		await twoAllowance.wait()
+		await _increaseAllowance(staker1, tokensAmount)
 
 		expect(() => staking.connect(staker1).stake(erc20Sft.address, tokensAmount))
 			.to.changeTokenBalances(erc20Sft, [staker1, staking], [-tokensAmount, tokensAmount])
 
-		expect(() => staking.connect(staker2).stake(erc20One.address, tokensAmount))
-			.to.changeTokenBalances(erc20One, [staker2, staking], [-tokensAmount, tokensAmount])
+		expect(() => staking.connect(staker1).stake(erc20One.address, tokensAmount))
+			.to.changeTokenBalances(erc20One, [staker1, staking], [-tokensAmount, tokensAmount])
 
-		expect(() => staking.connect(staker3).stake(erc20Two.address, tokensAmount))
-			.to.changeTokenBalances(erc20Two, [staker3, staking], [-tokensAmount, tokensAmount])
+		expect(() => staking.connect(staker1).stake(erc20Two.address, tokensAmount))
+			.to.changeTokenBalances(erc20Two, [staker1, staking], [-tokensAmount, tokensAmount])
 	})
 	it("possible to withdraw any tokens", async () => {
 		// mint {tokensAmount} SFT, ONE, TWO tokens to stakers
-		const staker1Sft = await erc20Sft.mint(staker1.address, tokensAmount)
-		await staker1Sft.wait()
-		const staker2One = await erc20One.mint(staker2.address, tokensAmount)
-		await staker2One.wait()
-		const staker3Two = await erc20Two.mint(staker3.address, tokensAmount)
-		await staker3Two.wait()
+		await mintTokens(staker1.address, tokensAmount)
 
-		const sftAllowance = await erc20Sft.connect(staker1).increaseAllowance(staking.address, tokensAmount)
-		await sftAllowance.wait()
-		const oneAllowance = await erc20One.connect(staker2).increaseAllowance(staking.address, tokensAmount)
-		await oneAllowance.wait()
-		const twoAllowance = await erc20Two.connect(staker3).increaseAllowance(staking.address, tokensAmount)
-		await twoAllowance.wait()
+		await _increaseAllowance(staker1, tokensAmount)
 
-		const staker1Stake = await staking.connect(staker1).stake(erc20Sft.address, tokensAmount)
-		await staker1Stake.wait()
-		const staker2Stake = await staking.connect(staker2).stake(erc20One.address, tokensAmount)
-		await staker2Stake.wait()
-		const staker3Stake = await staking.connect(staker3).stake(erc20Two.address, tokensAmount)
-		await staker3Stake.wait()
+		await _stake(staker1)
 
 		expect(() => staking.connect(staker1).withdraw(tokensAmount))
 			.to.changeTokenBalances(erc20Sft, [staker1, staking], [tokensAmount, -tokensAmount])
 
-		expect(() => staking.connect(staker2).withdraw(tokensAmount))
-			.to.changeTokenBalances(erc20One, [staker2, staking], [tokensAmount, -tokensAmount])
+		expect(() => staking.connect(staker1).withdraw(tokensAmount))
+			.to.changeTokenBalances(erc20One, [staker1, staking], [tokensAmount, -tokensAmount])
 
-		expect(() => staking.connect(staker3).withdraw(tokensAmount))
-			.to.changeTokenBalances(erc20Two, [staker3, staking], [tokensAmount, -tokensAmount])
+		expect(() => staking.connect(staker1).withdraw(tokensAmount))
+			.to.changeTokenBalances(erc20Two, [staker1, staking], [tokensAmount, -tokensAmount])
 	})
 	it("gives rewards", async () => {
 		// mint {tokensAmount} SFT tokens to stakers
-		const staker1Sft = await erc20Sft.mint(staker1.address, tokensAmount)
-		await staker1Sft.wait()
+		await mintTokens(staker1.address, tokensAmount)
 
-		const sftAllowance = await erc20Sft.connect(staker1).increaseAllowance(staking.address, tokensAmount)
-		await sftAllowance.wait()
+		await _increaseAllowance(staker1, tokensAmount)
 
-		const staker1Stake = await staking.connect(staker1).stake(erc20Sft.address, tokensAmount)
-		await staker1Stake.wait()
+		await _stake(staker1)
 
 		await delay(2000)
 
@@ -136,37 +101,58 @@ describe("StakingRewards", () => {
 	it("can be minted only by owner", async () => {
 		await expect(erc20Sft.connect(staker1).mint(staker1.address, 1e5)).to.be.reverted
 	})
+
+	// deploy any tokens
+	async function deployErc20(deployer, tokenName, tokenSign) {
+		const Erc20 = await ethers.getContractFactory("ERC20Updated", deployer)
+		const erc20 = await Erc20.deploy(tokenName, tokenSign)
+		await erc20.deployed()
+
+		return erc20
+	}
+
+	// read the balance of SFT, ONE, TWO tokens on the {staker}'s account
+	async function tokensBalance(staker) {
+		const stakerSft = await erc20Sft.balanceOf(staker)
+		const stakerOne = await erc20One.balanceOf(staker)
+		const stakerTwo = await erc20Two.balanceOf(staker)
+
+		console.log("Staker {", staker, "} SFT:", stakerSft, " ONE: ", stakerOne, " TWO: ", stakerTwo)
+
+		return [stakerSft, stakerOne, stakerTwo]
+	}
+
+	// mint SFT, ONE, TWO tokens to {staker} with {amount}
+	async function mintTokens(staker, amount) {
+		const sftMinted = await erc20Sft.mint(staker, amount)
+		await sftMinted.wait()
+		const oneMinted = await erc20One.mint(staker, amount)
+		await oneMinted.wait()
+		const twoMinted = await erc20Two.mint(staker, amount)
+		await twoMinted.wait()
+
+		const stakerBalance = tokensBalance(staker)
+
+		return stakerBalance
+	}
+
+	// increases allowance from {staker} to StakingRewards
+	async function _increaseAllowance(staker, amount) {
+		const sftAllowance = await erc20Sft.connect(staker).increaseAllowance(staking.address, amount)
+		await sftAllowance.wait()
+		const oneAllowance = await erc20One.connect(staker).increaseAllowance(staking.address, amount)
+		await oneAllowance.wait()
+		const twoAllowance = await erc20Two.connect(staker).increaseAllowance(staking.address, amount)
+		await twoAllowance.wait()
+	}
+
+	// stake SFT, ONE, TWO tokens to StakingRewards
+	async function _stake(staker) {
+		const stakeSft = await staking.connect(staker).stake(erc20Sft.address, tokensAmount)
+		await stakeSft.wait()
+		const stakeOne = await staking.connect(staker).stake(erc20One.address, tokensAmount)
+		await stakeOne.wait()
+		const stakeTwo = await staking.connect(staker).stake(erc20Two.address, tokensAmount)
+		await stakeTwo.wait()
+	}
 })
-
-async function mintTokens(staker, stakerNum, sft, one, two, amount) {
-	const sftMinted = await sft.mint(staker, amount)
-	await sftMinted.wait()
-	const oneMinted = await one.mint(staker, amount)
-	await oneMinted.wait()
-	const twoMinted = await two.mint(staker, amount)
-	await twoMinted.wait()
-
-	const stakerBalance = tokensBalance(staker, stakerNum, sft, one, two)
-
-	return stakerBalance
-}
-
-// read the balance of SFT, ONE, TWO tokens on the {staker}'s account
-async function tokensBalance(staker, stakerNum, sft, one, two) {
-	const stakerSft = await sft.balanceOf(staker)
-	const stakerOne = await one.balanceOf(staker)
-	const stakerTwo = await two.balanceOf(staker)
-
-	console.log("Staker", stakerNum, " SFT:", stakerSft, " ONE: ", stakerOne, " TWO: ", stakerTwo)
-
-	return [stakerSft, stakerOne, stakerTwo]
-}
-
-// deploy any tokens
-async function deployErc20(deployer, tokenName, tokenSign) {
-	const Erc20 = await ethers.getContractFactory("ERC20Updated", deployer)
-	const erc20 = await Erc20.deploy(tokenName, tokenSign)
-	await erc20.deployed()
-
-	return erc20
-}
