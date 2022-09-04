04d16bc77741ba181a554e2a49d48aad02d56845
r0uge-exe <pukhnobg@gmail.com>
2022-09-05 01:07:54 +0300

    added erc20 tests

--- test/staking.test.js
+++ test/staking.test.js
@@ -1,8 +1,10 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+// the amount of tokens
 let tokensAmount = 10
 
+// delay for increasing rewards amount
 let delay = ms => new Promise(res => setTimeout(res, ms))
 
 describe("StakingRewards", () => {
@@ -27,16 +29,19 @@ describe("StakingRewards", () => {
 		await staking.deployed()
 	})
 	it("deployed", async () => {
+		// check is addresses of contracts is proper
 		expect(staking.address).to.be.properAddress
 		expect(erc20Sft.address).to.be.properAddress
 		expect(erc20One.address).to.be.properAddress
 		expect(erc20Two.address).to.be.properAddress
 	})
 	it("have 0 any tokens by default", async () => {
+		// check stakers balance before every actions
 		const staker1balance = await tokensBalance(staker1.address)
 		const staker2balance = await tokensBalance(staker2.address)
 		const staker3balance = await tokensBalance(staker3.address)
 
+		// before every ation stakers token balance must be equal to zero
 		for (let i = 0; i < 3; ++i) {
 			expect(staker1balance[i]).to.eq(0)
 			expect(staker2balance[i]).to.eq(0)
@@ -44,8 +49,10 @@ describe("StakingRewards", () => {
 		}
 	})
 	it("possible to mint any tokens", async () => {
+		// mints {tokensAmount} tokens to {staker1}
 		const staker1BalanceAfterMint = await mintTokens(staker1.address, tokensAmount)
 
+		// {staker1} tokens balance must be equal to {tokensAmount} after mint
 		for (let i = 0; i < 3; ++i) {
 			expect(staker1BalanceAfterMint[i]).to.eq(tokensAmount)
 		}
@@ -54,14 +61,14 @@ describe("StakingRewards", () => {
 		// mint {tokensAmount} SFT, ONE, TWO tokens to stakers
 		await mintTokens(staker1.address, tokensAmount)
 
+		// increases allowance from {staker1} to StakingRewards contract
 		await _increaseAllowance(staker1, tokensAmount)
 
+		// after staking action StakingRewards contract must have all of staked tokens on account
 		expect(() => staking.connect(staker1).stake(erc20Sft.address, tokensAmount))
 			.to.changeTokenBalances(erc20Sft, [staker1, staking], [-tokensAmount, tokensAmount])
-
 		expect(() => staking.connect(staker1).stake(erc20One.address, tokensAmount))
 			.to.changeTokenBalances(erc20One, [staker1, staking], [-tokensAmount, tokensAmount])
-
 		expect(() => staking.connect(staker1).stake(erc20Two.address, tokensAmount))
 			.to.changeTokenBalances(erc20Two, [staker1, staking], [-tokensAmount, tokensAmount])
 	})
@@ -69,16 +76,17 @@ describe("StakingRewards", () => {
 		// mint {tokensAmount} SFT, ONE, TWO tokens to stakers
 		await mintTokens(staker1.address, tokensAmount)
 
+		// increases allowance from {staker1} to StakingRewards contract
 		await _increaseAllowance(staker1, tokensAmount)
 
+		// stakes all of 3 tokens to StakingRewards contract
 		await _stake(staker1)
 
+		// after withdraw action StakingRewards contract must have all of staked tokens on account
 		expect(() => staking.connect(staker1).withdraw(tokensAmount))
 			.to.changeTokenBalances(erc20Sft, [staker1, staking], [tokensAmount, -tokensAmount])
-
 		expect(() => staking.connect(staker1).withdraw(tokensAmount))
 			.to.changeTokenBalances(erc20One, [staker1, staking], [tokensAmount, -tokensAmount])
-
 		expect(() => staking.connect(staker1).withdraw(tokensAmount))
 			.to.changeTokenBalances(erc20Two, [staker1, staking], [tokensAmount, -tokensAmount])
 	})
@@ -86,20 +94,23 @@ describe("StakingRewards", () => {
 		// mint {tokensAmount} SFT tokens to stakers
 		await mintTokens(staker1.address, tokensAmount)
 
+		// increases allowance from {staker1} to StakingRewards contract
 		await _increaseAllowance(staker1, tokensAmount)
 
+		// stakes all of 3 tokens to StakingRewards contract
 		await _stake(staker1)
 
+		// 2 seconds delay for increasing rewards amount
 		await delay(2000)
 
+		// after getting rewards from StakingRewards contract {staker1}'s SFT balance must be 
+		// increased by the amount that the staker has earned during this time
 		expect(() => staking.connect(staker1).getRewards())
 			.to.changeTokenBalances(erc20Sft, [staker1, staking], [tokensAmount, -tokensAmount])
 	})
 	it("sets the duration only by owner", async () => {
+		// only owner can set the duration of StakingRewards process
 		await expect(staking.connect(staker1).setDuration(10000000)).to.be.reverted
-	})
-	it("can be minted only by owner", async () => {
-		await expect(erc20Sft.connect(staker1).mint(staker1.address, 1e5)).to.be.reverted
 	})
 
 	// deploy any tokens
@@ -154,5 +165,41 @@ describe("StakingRewards", () => {
 		await stakeOne.wait()
 		const stakeTwo = await staking.connect(staker).stake(erc20Two.address, tokensAmount)
 		await stakeTwo.wait()
+	}
+})
+
+describe("ERC20Updated", async () => {
+
+	let staker
+	let erc20
+	beforeEach(async () => {
+		// takes 4 first addresses from hardhat network
+		[owner, staker] = await ethers.getSigners()
+
+		// deploy {TKN}
+		erc20 = await deployErc20(owner, "Token", "TKN")
+	})
+
+	it("can be minted only by owner", async () => {
+		// only owner can mint tokens to stakers
+		await expect(erc20.connect(staker).mint(staker.address, 1e5)).to.be.reverted
+	})
+
+	it("can be burned only by owner", async () => {
+		// mint some tokens to {staker}
+		const mintTokens = await erc20.mint(staker.address, 1e5)
+		await mintTokens.wait()
+
+		// only owner can mint tokens to stakers
+		await expect(erc20.connect(staker).burn(staker.address, 1e5)).to.be.reverted
+	})
+
+	// deploy any tokens
+	async function deployErc20(deployer, tokenName, tokenSign) {
+		const Erc20 = await ethers.getContractFactory("ERC20Updated", deployer)
+		const erc20 = await Erc20.deploy(tokenName, tokenSign)
+		await erc20.deployed()
+
+		return erc20
 	}
 })
